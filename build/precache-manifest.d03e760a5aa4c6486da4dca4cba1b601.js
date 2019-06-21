bd61ccc7bce8776f8b0512b7436f0c8bcd6bd0bf
Nikita <cherevaty.nik@gmail.com>
2019-06-21 20:46:49 +0300

    NikitaCherevatyi

---
+++ build/precache-manifest.d03e760a5aa4c6486da4dca4cba1b601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aec49a77d8e76b6a80481bb4990e0ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fccf0bd71813024c4e78",
+    "url": "./static/css/main.ca25720e.chunk.css"
+  },
+  {
+    "revision": "265a8762636d2391b955",
+    "url": "./static/js/2.61801948.chunk.js"
+  },
+  {
+    "revision": "fccf0bd71813024c4e78",
+    "url": "./static/js/main.116b4d6f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
