455e972b923703209294d376a406ce66616c2b32
Onisko <Onisko@MacBook.local>
2019-06-21 16:52:17 +0300

    solution

---
+++ build/precache-manifest.d805bbf3f6c1b9bed92c9d221cfcabcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e9199d3e1955642fbc8f17523b604c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb6432677cf2a9c32800",
+    "url": "./static/css/main.5fd3412d.chunk.css"
+  },
+  {
+    "revision": "bf616368a0c1e8b99f6b",
+    "url": "./static/js/2.746eda39.chunk.js"
+  },
+  {
+    "revision": "bb6432677cf2a9c32800",
+    "url": "./static/js/main.50b44903.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
