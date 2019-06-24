b95ff27bceaccf4d16433827825be7bc783135a9
yojikyo <yojikyo@gmail.com>
2019-06-24 09:17:08 +0300

    'build'

---
+++ build/precache-manifest.e4edc44e5630bade7c6d56d651cb7d64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e40b86ecaaabd0b8137414f5f2c3ddc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f543df789ecbae13b50",
+    "url": "./static/css/main.cebeb8b8.chunk.css"
+  },
+  {
+    "revision": "fded233d39fcfd46e425",
+    "url": "./static/js/2.2077ebaf.chunk.js"
+  },
+  {
+    "revision": "2f543df789ecbae13b50",
+    "url": "./static/js/main.6a5b3274.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
