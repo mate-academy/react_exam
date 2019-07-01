c50c355ca80be364f0956830caaba3631078490f
Olegus <gulldan1996@gmail.com>
2019-07-01 15:31:51 +0300

    a

---
+++ build/precache-manifest.b679d12f8513123311fb90d4cde7ef77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff34d91fe7348a6083b7d9407a90217c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6de26c198f635abbe2a3",
+    "url": "./static/css/main.fc4a8043.chunk.css"
+  },
+  {
+    "revision": "be6613b599cc10763e78",
+    "url": "./static/js/2.ea62c253.chunk.js"
+  },
+  {
+    "revision": "6de26c198f635abbe2a3",
+    "url": "./static/js/main.4dc22c4b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
